a0e464437ed3e40dd335319135aedf06d24dac31
Fita Sheila <90299098+fitashe@users.noreply.github.com>
2024-01-05 19:37:38 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -1,3 +1,4 @@
+# import library
 import itertools
 import pandas as pd
 import numpy as np
@@ -8,9 +9,11 @@ import time
 import pickle
 import joblib
 
+# membaca data
 with open("data/hungarian.data", encoding='Latin1') as file:
   lines = [line.strip() for line in file]
 
+# pemrosesan data
 data = itertools.takewhile(
   lambda x: len(x) == 76,
   (' '.join(lines[i:(i + 10)]).split() for i in range(0, len(lines), 10))
@@ -24,6 +27,7 @@ df = df.astype(float)
 
 df.replace(-9.0, np.NaN, inplace=True)
 
+# seleksi kolom dataframe
 df_selected = df.iloc[:, [1, 2, 7, 8, 10, 14, 17, 30, 36, 38, 39, 42, 49, 56]]
 
 column_mapping = {
@@ -87,10 +91,12 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
+# memuat model machine learning yang telah dilatih
 model = pickle.load(open("model/xgb_model.pkl", 'rb'))
 # with open("model/knn_model.pkl", 'rb') as model_file:
 #     model = pickle.load(model_file)
 
+# melakukan prediksi dan evaluasi akurasi model
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
 accuracy = round((accuracy * 100), 2)
@@ -101,13 +107,16 @@ df_final['target'] = y
 # ========================================================================================================================================================================================
 
 # STREAMLIT
+# konfigurasi aplikasi streamlit
 st.set_page_config(
   page_title = "Hungarian Heart Disease",
   page_icon = ":heart:"
 )
 
+# judul webpage
 st.title("Hungarian Heart Disease")
 
+# menambahkan gambar
 st.image('heart.jpg', width=600)
 
 st.write(f"**_Model's Accuracy_** :  :green[**{91.4}**]%")
@@ -115,9 +124,11 @@ st.write("")
 
 tab1, tab2 = st.tabs(["Single-predict", "Multi-predict"])
 
+# konten untuk single prediksi
 with tab1:
   st.sidebar.header("**User Input** Sidebar")
 
+  # mengatur style css 
   st.markdown(
       """
       <style>
@@ -262,9 +273,12 @@ with tab1:
 
   st.write("")
   st.write("")
+
+  # menampilkan hasil prediksi
   st.subheader("Prediction:")
   st.subheader(result)
 
+# konten untuk memprediksi multiple data
 with tab2:
   st.header("Predict multiple data:")
 
